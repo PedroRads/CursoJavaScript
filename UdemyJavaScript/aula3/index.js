f8454d2f8af90ffd910a0333a7325502d7dba90b
PedroRads <112221185+PedroRads@users.noreply.github.com>
2022-09-03 20:31:17 -0400

    Add files via upload

---
+++ UdemyJavaScript/aula3/index.js
@@ -0,0 +1,1 @@
+console.log('Meu Nome é "Pedro". Estou aprendendo JavaScript às 10 da Manhã.');
---
+++ UdemyJavaScript/aula4/comentarios.js
@@ -0,0 +1,16 @@
+// escrever comentario
+console.log("salve");
+//comentario 2
+/*
+console.log("1");
+
+console.log("5");
+*/
+
+   /*
+comentario por bloco
+bloco 
+bloco
+*/
+//console.log("4");
+console.log("2");
